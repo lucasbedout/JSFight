7b4333b6587cb94bca92df73dae445fbdfbcc9ba
lucasbedout <lucas.bedout@free.fr>
2015-05-15 20:49:31 +0200

    archi

--- public/src/js/controllers/fighterCtrl.js
+++ public/src/js/controllers/fighterCtrl.js
@@ -1,25 +1,26 @@
-var player = { name: 'Mehdi', life: 100, posX: 0, posY: 0, image: 0 };
+angular.module('JSFight').controller('fighterCtrl', ['$scope', 'Fight', function($scope, Fight) {
 
-angular.module('JSFight').controller('initiateGameCtrl', function(){
-		this.player1 = player;
-		player.name = 'Lucas';
-		this.player2 = player;
-    });
+        $scope.player = {
+            name: 'Mehdi',
+            life: 100,
+            posX: 0,
+            posY: 0,
+            image: 0
+        };
 
-angular.module('JSFight').controller('actionGameCtrl', function(){
-		//send to node the key
-		/*$(document).ready(function(){
-			var user,key;
-			$("#submit").click(function(){
-				user=this.player1.name;
-				key='a';
-				$.post("http://localhost:3000/game",{user: user,key: key}, function(data){
-				});
-			});
-		});*/
-    });
+        //send to node the key
+        /*$(document).ready(function(){
+         var user,key;
+         $("#submit").click(function(){
+         user=this.player1.name;
+         key='a';
+         $.post("http://localhost:3000/game",{user: user,key: key}, function(data){
+         });
+         });
+         });*/
 
-angular.module('JSFight').controller('refreshGameCtrl', function(){
-		//receive the instruction by json
-		//print the stickman
-    });
+        $scope.sendPlayer = function() {
+            // Use Fight service to send something
+        };
+
+    }]);
---
+++ public/src/js/services/Fight.js
@@ -0,0 +1,11 @@
+// public/src/js/services/TestService.js
+angular.module('JSFight')
+
+    .factory('Fight', ['$http', 'API', function($http, API) {
+
+        return {
+
+            // API calls
+        };
+
+    }]);
