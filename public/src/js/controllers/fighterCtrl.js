5eba1ea6b269e0e052840db6ed9be92ef18be7ce
inceptor <olivier.aqwzsx@laposte.net>
2015-05-16 03:20:43 +0200

    update core game bis

--- public/src/js/controllers/fighterCtrl.js
+++ public/src/js/controllers/fighterCtrl.js
@@ -18,6 +18,14 @@ angular.module('JSFight').controller('fighterCtrl', ['$scope', '$rootScope', 'Fi
          });
          });
          });*/
+		 
+		$scope.key = 'none'
+
+		$rootScope.$on('keypress', function (evt, obj, key) {
+			$scope.$apply(function () {
+				$scope.key = key;
+			});
+		})
 
         $scope.sendPlayer = function() {
             // Use Fight service to send something
@@ -25,3 +33,17 @@ angular.module('JSFight').controller('fighterCtrl', ['$scope', '$rootScope', 'Fi
         };
 
     }]);
+	
+angular.module('JSFight').directive('keypressEvents',
+
+	function ($document, $rootScope) {
+		return {
+			restrict: 'A',
+			link: function () {
+				console.log('linked');
+				$document.bind('keypress', function (e) {
+					$rootScope.$broadcast('keypress', e, String.fromCharCode(e.which));
+				});
+			}
+		}
+});
